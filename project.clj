c6afe22bf8c02c3df05c56a826c103dd6b8b19e5
Isak Sky <isak.sky@gmail.com>
2014-01-15 20:41:07 -0800

    update solr to 4.6.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject solrclj "0.4.2-SNAPSHOT"
+(defproject solrclj "0.4.3-SNAPSHOT"
   :description "A clojure library for using Apache Solr."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.apache.solr/solr-solrj "4.3.1"]
+                 [org.apache.solr/solr-solrj "4.6.0"]
                  [org.slf4j/jcl-over-slf4j "1.7.5"]
                  [ch.qos.logback/logback-classic "1.0.13"]]
-  :dev-dependencies [[org.apache.solr/solr-core "4.3.1"]
+  :dev-dependencies [[org.apache.solr/solr-core "4.6.0"]
                                   [javax.servlet/servlet-api "2.5"]
                                   [commons-io/commons-io "2.4"]
                                   [org.mortbay.jetty/jetty "6.1.26"]]
